01ff160ef24956dc43dbfd674b86c715ca11db97
Gary Hake <Gary.Hake@waystar.com>
2021-01-11 13:55:46 -0500

    serving files statically

--- 03-expressJS/app.js
+++ 03-expressJS/app.js
@@ -8,6 +8,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+// allows us to use static (styles, scripts) files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // we've created our own middleware!
 app.use('/admin', adminRoutes);
