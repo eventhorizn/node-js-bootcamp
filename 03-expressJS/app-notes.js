6925da0a45fda4025724b0d5db7d924c943cc6c9
Gary Hake <Gary.Hake@waystar.com>
2021-01-11 10:10:56 -0500

    parsing incoming requests

---
+++ 03-expressJS/app-notes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express(); // valid request handler
+
+// every request will go through this function
+// next is another function
+// top to bottom if you call next
+// if you don't call next, return the response
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next();
+});
+
+app.use('/', () => {
+    console.log('This always runs');
+    next();
+});
+
+app.use('/add-product', (req, res, next) => {
+    //console.log('In another middleware');
+    res.send('<h1>The "Add Product" Page</h1>');
+});
+
+// '/' is default
+// We don't call next above, and the filter would match this
+// so we put it last. If it was first it would ALWAYS be hit
+app.use('/', (req, res, next) => {
+    //console.log('In another middleware');
+    res.send('<h1>Hello from Express!</h1>');
+});
+
+// creates our server
+app.listen(3000);
--- 03-expressJS/app.js
+++ 03-expressJS/app.js
@@ -1,33 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express(); // valid request handler
+const app = express();
 
-// every request will go through this function
-// next is another function
-// top to bottom if you call next
-// if you don't call next, return the response
-// app.use((req, res, next) => {
-//     console.log('In the middleware');
-//     next();
-// });
-
-app.use('/', () => {
-    console.log('This always runs');
-    next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/add-product', (req, res, next) => {
-    //console.log('In another middleware');
-    res.send('<h1>The "Add Product" Page</h1>');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-// '/' is default
-// We don't call next above, and the filter would match this
-// so we put it last. If it was first it would ALWAYS be hit
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
 app.use('/', (req, res, next) => {
-    //console.log('In another middleware');
     res.send('<h1>Hello from Express!</h1>');
 });
 
-// creates our server
 app.listen(3000);
