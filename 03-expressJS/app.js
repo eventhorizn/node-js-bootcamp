65d27bc292264df0e9696d1f249d8911a750dbcb
Gary Hake <Gary.Hake@waystar.com>
2021-01-11 10:28:07 -0500

    filtering paths

--- 03-expressJS/app.js
+++ 03-expressJS/app.js
@@ -9,8 +9,12 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // we've created our own middleware!
-app.use(adminRoutes);
-
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+// catch all
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1');
+});
 
 app.listen(3000);
