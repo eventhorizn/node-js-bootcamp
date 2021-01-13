f920fcc39b0cdb65401b16ccdc7d077ec261737c
Gary Hake <Gary.Hake@waystar.com>
2021-01-12 21:53:08 -0500

    file cleanup

--- 05-mvc/routes/shop.js
+++ 05-mvc/routes/shop.js
@@ -1,13 +1,8 @@
-const path = require('path');
 const express = require('express');
-const rootDir = require('../util/path');
 const router = express.Router();
 const adminData = require('./admin');
 
-// get does an exact match
 router.get('/', (req, res, next) => {
-	//console.log(adminData.products);
-	//res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 	const products = adminData.products;
 	res.render('shop', {
 		prods: products,
