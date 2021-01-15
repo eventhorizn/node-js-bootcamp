3227e783988863ace3667d4eb5b544acab88d5ad
Gary Hake <Gary.Hake@waystar.com>
2021-01-15 08:48:57 -0500

    starting delete

--- 07-dynamic-routes-advanced-models/models/product.js
+++ 07-dynamic-routes-advanced-models/models/product.js
@@ -48,6 +48,17 @@ module.exports = class Product {
 		});
 	}
 
+	static deleteById(id) {
+		getProductsFromFile((products) => {
+			const updatedProducts = products.filter((p) => +p.id !== +id);
+
+			fs.write(p, JSON.stringify(updatedProducts), (err) => {
+				if (!err) {
+				}
+			});
+		});
+	}
+
 	static fetchAll(cb) {
 		getProductsFromFile(cb);
 	}
