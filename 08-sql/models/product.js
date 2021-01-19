34014e82d17809f306b2753d9efc8aac914f71b1
Gary Hake <Gary.Hake@waystar.com>
2021-01-19 08:24:02 -0500

    cart ported over to sequelize

--- 08-sql/models/product.js
+++ 08-sql/models/product.js
@@ -1,4 +1,4 @@
-const db = require('../util/database.js');
+const db = require('../util/database');
 
 const Cart = require('./cart');
 
--- 09-sequelize/app.js
+++ 09-sequelize/app.js
@@ -6,6 +6,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -34,18 +36,30 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem }); // many to many
+Product.belongsToMany(Cart, { through: CartItem }); // many to many
 
 sequelize
 	//.sync({ force: true })
 	.sync()
 	.then(() => {
-		User.findByPk(1);
+		return User.findByPk(1);
 	})
 	.then((user) => {
 		if (!user) {
 			return User.create({ name: 'Gary', email: 'test@test.com' });
 		}
 		return user;
+	})
+	.then((user) => {
+		return user.getCart().then((cart) => {
+			if (!cart) {
+				return user.createCart();
+			}
+			return cart;
+		});
 	})
 	.then(() => {
 		app.listen(3000);
