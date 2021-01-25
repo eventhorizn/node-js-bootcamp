65b12371dad64e55a1e949a2bc488c6cba544ab7
Gary Hake <Gary.Hake@waystar.com>
2021-01-25 09:07:55 -0500

    keeping user data on validation error

--- 14-validation/controllers/auth.js
+++ 14-validation/controllers/auth.js
@@ -42,12 +42,26 @@ exports.getSignup = (req, res, next) => {
 		path: '/signup',
 		pageTitle: 'Signup',
 		errorMessage: message,
+		oldInput: {
+			email: '',
+			password: '',
+			confirmPassword: '',
+		},
 	});
 };
 
 exports.postLogin = (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(422).render('auth/login', {
+			path: '/login',
+			pageTitle: 'Login',
+			errorMessage: errors.array()[0].msg,
+		});
+	}
 
 	User.findOne({ email: email })
 		.then((user) => {
@@ -83,13 +97,17 @@ exports.postSignup = (req, res, next) => {
 	const email = req.body.email;
 	const password = req.body.password;
 	const errors = validationResult(req);
-	console.log(errors);
 
 	if (!errors.isEmpty()) {
 		return res.status(422).render('auth/signup', {
 			path: '/signup',
 			pageTitle: 'Signup',
 			errorMessage: errors.array()[0].msg,
+			oldInput: {
+				email: email,
+				password: password,
+				confirmPassword: req.body.confirmPassword,
+			},
 		});
 	}
 
