23beb9f4fba3de0504ff9fecb759f186b015c6e3
Gary Hake <Gary.Hake@waystar.com>
2021-02-07 17:27:23 -0500

    updating posts

--- 18-1-rest-api/controllers/feed.js
+++ 18-1-rest-api/controllers/feed.js
@@ -1,5 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
 const { validationResult } = require('express-validator');
-const post = require('../models/post');
 
 const Post = require('../models/post');
 
@@ -85,5 +87,62 @@ exports.getPost = (req, res, next) => {
 		});
 };
 
-//update post
-//imageUrl = req.file.path.replace("\\","/");
+exports.updatePost = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		const error = new Error('Validation failed, entered data is incorrect');
+		error.statusCode = 422;
+
+		throw error;
+	}
+
+	const postId = req.params.postId;
+	const title = req.body.title;
+	const content = req.body.content;
+	let imageUrl = req.body.image;
+
+	if (req.file) {
+		imageUrl = req.file.path.replace('\\', '/');
+	}
+
+	if (!imageUrl) {
+		const error = new Error('No file picked');
+		error.statusCode = 422;
+
+		throw error;
+	}
+
+	Post.findById(postId)
+		.then((post) => {
+			if (!post) {
+				const error = new Error('Could not find post.');
+				error.statusCode = 404;
+				throw error; //throws to catch
+			}
+
+			if (imageUrl !== post.imageUrl) {
+				clearImage(post.imageUrl);
+			}
+
+			post.title = title;
+			post.imageUrl = imageUrl;
+			post.content = content;
+
+			return post.save();
+		})
+		.then((result) => {
+			res.status(200).json({ message: 'Post updated', post: result });
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+
+			next();
+		});
+};
+
+const clearImage = (filePath) => {
+	filePath = path.join(__dirname, '..', filePath);
+	fs.unlink(filePath, (err) => console.log(err));
+};
