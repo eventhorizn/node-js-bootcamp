1d2334e5a5b6202223c276f455fa784eca719aa5
Gary Hake <Gary.Hake@waystar.com>
2021-02-07 11:19:08 -0500

    static images and error handling

--- 18-1-rest-api/app.js
+++ 18-1-rest-api/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -11,6 +13,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader(
@@ -23,6 +27,14 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.statusCode || 500;
+	const message = error.message;
+
+	res.status(status).json({ message: message });
+});
 
 mongoose
 	.connect(process.env.MONGODB_URI, {
--- 18-1-rest-api/controllers/feed.js
+++ 18-1-rest-api/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
 	res.status(200).json({
 		posts: [
@@ -20,26 +22,35 @@ exports.getPosts = (req, res, next) => {
 exports.postPost = (req, res, next) => {
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		return res
-			.status(422)
-			.json({
-				message: 'Validation failed, entered data is incorrect',
-				errors: errors.array(),
-			});
+		const error = new Error('Validation failed, entered data is incorrect');
+		error.statusCode = 422;
+
+		throw error;
 	}
 
 	const title = req.body.title;
 	const content = req.body.content;
 
-	//create in db
-	res.status(201).json({
-		message: 'Post created succesfully',
-		post: {
-			_id: Date.now(),
-			title: title,
-			content: content,
-			creator: { name: 'Gary' },
-			createdAt: new Date(),
-		},
+	const post = new Post({
+		title: title,
+		content: content,
+		imageUrl: 'images/book.jpg',
+		creator: { name: 'Gary' },
 	});
+
+	post
+		.save()
+		.then((result) => {
+			res.status(201).json({
+				message: 'Post created succesfully',
+				post: result,
+			});
+		})
+		.catch((err) => {
+			if (!err.statusCode) {
+				err.statusCode = 500;
+			}
+
+			next(err);
+		});
 };
