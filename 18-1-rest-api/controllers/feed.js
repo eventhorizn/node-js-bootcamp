34aceea3d00cf17ed403650bfc0c20b41664ed70
Gary Hake <Gary.Hake@waystar.com>
2021-02-07 10:34:21 -0500

    setting up post connection b/t api and front end
    adding server side validation

--- 18-1-rest-api/controllers/feed.js
+++ 18-1-rest-api/controllers/feed.js
@@ -1,16 +1,45 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
 	res.status(200).json({
-		posts: [{ title: 'First Post', content: 'This is the first post' }],
+		posts: [
+			{
+				_id: '1',
+				title: 'First Post',
+				content: 'This is the first post',
+				imageUrl: 'images/book.jpg',
+				creator: {
+					name: 'Gary',
+				},
+				createdAt: new Date(),
+			},
+		],
 	});
 };
 
 exports.postPost = (req, res, next) => {
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res
+			.status(422)
+			.json({
+				message: 'Validation failed, entered data is incorrect',
+				errors: errors.array(),
+			});
+	}
+
 	const title = req.body.title;
 	const content = req.body.content;
 
 	//create in db
 	res.status(201).json({
 		message: 'Post created succesfully',
-		post: { id: Date.now(), title: title, content: content },
+		post: {
+			_id: Date.now(),
+			title: title,
+			content: content,
+			creator: { name: 'Gary' },
+			createdAt: new Date(),
+		},
 	});
 };
