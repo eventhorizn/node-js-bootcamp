2168c9e39b18f4617f3e997681e9dcff1888044e
Gary Hake <Gary.Hake@waystar.com>
2021-02-11 20:40:00 -0500

    deleting a post

--- 20-graph-ql/api/app.js
+++ 20-graph-ql/api/app.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -12,6 +11,7 @@ const { graphqlHTTP } = require('express-graphql');
 const graphqlSchema = require('./graphql/schema');
 const graphqlResolver = require('./graphql/resolvers');
 const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const app = express();
 
@@ -123,8 +123,3 @@ mongoose
 	.catch((err) => {
 		console.log(err);
 	});
-
-const clearImage = (filePath) => {
-	filePath = path.join(__dirname, '..', filePath);
-	fs.unlink(filePath, (err) => console.log(err));
-};
